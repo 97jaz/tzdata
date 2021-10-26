c9e824ccf0cb667651c67e8d9ab47eabf14bfb19
Jon Zeppieri <zeppieri@gmail.com>
2021-10-26 12:39:29 -0400

    updates tzdb to 2021e

--- tzinfo/info.rkt
+++ tzinfo/info.rkt
@@ -3,6 +3,6 @@
 ;; `tzdata-zoneinfo-dir` is for backward compatibility with
 ;; older versions of tzinfo. It can be removed in future
 ;; versions.
-(define iana-tz-version "2019c")
+(define iana-tz-version "2021e")
 (define tzdata-zoneinfo-dir "tzdata/zoneinfo")
 (define tzdata-zoneinfo-module-path (quote (lib "tzinfo/tzdata/zoneinfo")))
