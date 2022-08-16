5594a7bef021a4aed3f949fe0ef7d51fb7659728
Jon Zeppieri <zeppieri@gmail.com>
2022-08-15 21:39:38 -0400

    tzdb 2022c

--- tzinfo/info.rkt
+++ tzinfo/info.rkt
@@ -3,7 +3,7 @@
 ;; `tzdata-zoneinfo-dir` is for backward compatibility with
 ;; older versions of tzinfo. It can be removed in future
 ;; versions.
-(define iana-tz-version "2022a")
+(define iana-tz-version "2022c")
 (define tzdata-zoneinfo-dir "tzdata/zoneinfo")
 (define tzdata-zoneinfo-module-path (quote (lib "tzinfo/tzdata/zoneinfo")))
 (define copy-shared-files (quote ("tzdata")))
